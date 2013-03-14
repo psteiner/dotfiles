3139d83282ff34f87b737c7b0ec1ec811fa3415e
Philip Steiner <philip@psteiner.com>
2013-03-14 03:47:15 +0000

    Add dircolors and only create symlinks if they don't already exist

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,28 +11,32 @@ dotdir=~/dotfiles                    # dotfiles directory
 olddir=~/oldfiles             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="bash_aliases bash_login bash_logout bash_profile bashrc gemrc gitconfig gitignore_global inputrc irbrc minttyrc vimrc"    
+files="bash_aliases bash_login bash_logout bash_profile bashrc gemrc gitconfig gitignore_global inputrc irbrc minttyrc vimrc dircolors" 
 
 ##########
 
 # create olddir in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+if [ ! -d $olddir ]; then 
+  echo "Creating $olddir for backup of any existing dotfiles in ~"
+  mkdir -p $olddir; 
+fi;
 
 # change to the dotfiles directory
-echo "Changing to the $dotdir directory"
 cd $dotdir
-echo "...done"
 
 # move any existing dotfiles in homedir to olddir, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 
 for file in $files; do
+  # back up existing dot files
   if [ -e ~/.$file ] && [ ! -h ~/.$file ]; then
     echo "Moving ~/.$file to $olddir"
     mv ~/.$file $olddir/
   fi
-  echo "Creating symlink to $file in home directory."
-  ln -s $dotdir/$file ~/.$file
+
+  if [ ! -h ~/.$file ]; then
+
+    echo "Creating symlink to $file in home directory."
+    ln -s $dotdir/$file ~/.$file
+  fi
 done
 
